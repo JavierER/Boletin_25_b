c082969184715e43710ac29ff5bed604a5550cab
Usuario <Usuario@192.168.0.154>
2016-06-10 23:39:28 +0200

    commit

---
+++ src/boletin25/b/Boletin25B.java
@@ -0,0 +1,16 @@
+
+package boletin25.b;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin25B {
+
+    
+    public static void main(String[] args) {
+       Frame window= new Frame();
+       window.setVisible(true);
+    }
+    
+}
